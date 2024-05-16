e763de66f1f0f005a4782e9ff5e42a9c723972d4
Rushirufruu <vicente.zapatasilva@gmail.com>
2024-05-16 15:08:17 -0400

    Se agrega la pagina crear factura

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login';
+import CrearFactura from './Pages/CrearFactura';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-        <Route path='/' element={<Login/>}/>
+        <Route path='/' element={<CrearFactura/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Pages/CrearFactura.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PaginaCrearFactura from '../Components/PaginaLogin/PaginaCrearFactura/PaginaCrearFactura'
+const CrearFactura = () => {
+  return (
+    <div>
+        <PaginaCrearFactura/>
+    </div>
+  )
+}
+
+export default CrearFactura
