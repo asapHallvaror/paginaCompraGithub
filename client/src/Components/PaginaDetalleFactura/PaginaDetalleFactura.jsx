d098b7a1c902c87c616d80aa3928134489b1a67f
Álvaro Morales <alvaromoralesaguirre13@gmail.com>
2024-06-05 18:08:27 -0400

    Detalles factura bonitos y centrados

--- client/src/Components/PaginaDetalleFactura/PaginaDetalleFactura.jsx
+++ client/src/Components/PaginaDetalleFactura/PaginaDetalleFactura.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import './PaginaDetalleFactura.css';
 
 const PaginaDetFac = () => {
@@ -32,29 +32,102 @@ const PaginaDetFac = () => {
     const fechaFormateada = fecha.toLocaleDateString('es-ES', opciones);
 
     return (
-        <div>
+        <div className='detalle-container'>
+            
             <h1>Detalles de la Factura N° {factura.numero_orden}</h1>
-            <p>Número de Factura: {factura.numero_orden}</p>
-            <p>Fecha de Factura: {fechaFormateada}</p>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Número de Factura:</th>
+                        <td>{factura.numero_orden}</td>
+                    </tr>
+                    <tr>
+                        <th>Fecha de Factura:</th>
+                        <td>{fechaFormateada}</td>
+                    </tr>
+                </tbody>
+            </table>
             <h2>Datos del proveedor</h2>
-            <p>Rut: {factura.rut_proveedor}</p>
-            <p>Razón social: {factura.razon_social_proveedor}</p>
-            <p>Dirección: {factura.direccion_proveedor}</p>
-            <p>Teléfono: +56{factura.telefono_proveedor}</p>
-            <p>Correo: {factura.correo_proveedor}</p>
-            <p>Sitio web: {factura.sitio_web_proveedor}</p>
-            <p>Tipo de servicio: {factura.tipo_servicio}</p>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Rut:</th>
+                        <td>{factura.rut_proveedor}</td>
+                    </tr>
+                    <tr>
+                        <th>Razón social:</th>
+                        <td>{factura.razon_social_proveedor}</td>
+                    </tr>
+                    <tr>
+                        <th>Dirección:</th>
+                        <td>{factura.direccion_proveedor}</td>
+                    </tr>
+                    <tr>
+                        <th>Teléfono:</th>
+                        <td>+56{factura.telefono_proveedor}</td>
+                    </tr>
+                    <tr>
+                        <th>Correo:</th>
+                        <td>{factura.correo_proveedor}</td>
+                    </tr>
+                    <tr>
+                        <th>Sitio web:</th>
+                        <td>{factura.sitio_web_proveedor}</td>
+                    </tr>
+                    <tr>
+                        <th>Tipo de servicio:</th>
+                        <td>{factura.tipo_servicio}</td>
+                    </tr>
+                </tbody>
+            </table>
             <h2>Datos del cliente</h2>
-            <p>Rut: {factura.rut_cliente}</p>
-            <p>Nombre/Razón social: {factura.nombre_cliente}</p>
-            <p>Dirección: {factura.direccion_cliente}</p>
-            <p>Teléfono: {factura.telefono_cliente}</p>
-            <p>Correo: {factura.correo_cliente}</p>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Rut:</th>
+                        <td>{factura.rut_cliente}</td>
+                    </tr>
+                    <tr>
+                        <th>Nombre/Razón social:</th>
+                        <td>{factura.nombre_cliente}</td>
+                    </tr>
+                    <tr>
+                        <th>Dirección:</th>
+                        <td>{factura.direccion_cliente}</td>
+                    </tr>
+                    <tr>
+                        <th>Teléfono:</th>
+                        <td>{factura.telefono_cliente}</td>
+                    </tr>
+                    <tr>
+                        <th>Correo:</th>
+                        <td>{factura.correo_cliente}</td>
+                    </tr>
+                </tbody>
+            </table>
             <h2>Montos</h2>
-            <p>Subtotal: {factura.subtotal}</p>
-            <p>IVA: {factura.iva}</p>
-            <p>Total General: {factura.total_general}</p>
-            {/* Add more fields as necessary */}
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Subtotal:</th>
+                        <td>{Number(factura.subtotal).toLocaleString('es-CL', { style: 'currency', currency: 'CLP' })}</td>
+                    </tr>
+                    <tr>
+                        <th>IVA:</th>
+                        <td>{Number(factura.iva).toLocaleString('es-CL', { style: 'currency', currency: 'CLP' })}</td>
+                    </tr>
+                    <tr>
+                        <th>Total General:</th>
+                        <td>{Number(factura.total).toLocaleString('es-CL', { style: 'currency', currency: 'CLP' })}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <Link to='/home' >
+                <button className='btn-volver'>
+                    <svg height="16" width="16" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1024 1024"><path d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z"></path></svg>
+                    <span>Volver</span>
+                </button>
+            </Link>
         </div>
     );
 };
